1cb695a08205095919205a79f61349e2ed47b7d4
Nathania <nathania.kayla18@gmail.com>
2025-06-04 12:16:24 +0700

    PERTEMUAN 14

--- app/Http/Controllers/PegawaiController.php
+++ app/Http/Controllers/PegawaiController.php
@@ -5,15 +5,18 @@ namespace App\Http\Controllers;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\DB;
 
+
+
 class PegawaiController extends Controller
 {
+    //
     public function index()
 	{
     	// mengambil data dari table pegawai
-		$pegawai = DB::table('pegawai')->get();
+		$pegawai = DB::table('pegawai')->paginate(10);
 
     	// mengirim data pegawai ke view index
-		return view('pegawai.index',['pegawai' => $pegawai]);
+		return view('index2',['pegawai' => $pegawai]);
 
 	}
 
@@ -28,15 +31,18 @@ class PegawaiController extends Controller
 
 	// method untuk insert data ke table pegawai
 	public function store(Request $request)
-{
-    DB::table('pegawai')->insert([
-        'pegawai_nama' => $request->nama,
-        'pegawai_jabatan' => $request->jabatan,
-        'pegawai_umur' => $request->umur,
-        'pegawai_alamat' => $request->alamat
-    ]);
-    return redirect('/pegawai');
-}
+	{
+		// insert data ke table pegawai
+		DB::table('pegawai')->insert([
+			'pegawai_nama' => $request->nama,
+			'pegawai_jabatan' => $request->jabatan,
+			'pegawai_umur' => $request->umur,
+			'pegawai_alamat' => $request->alamat
+		]);
+		// alihkan halaman ke halaman pegawai
+		return redirect('/pegawai');
+
+	}
 
 	// method untuk edit data pegawai
 	public function edit($id)
@@ -45,7 +51,6 @@ class PegawaiController extends Controller
 		$pegawai = DB::table('pegawai')->where('pegawai_id',$id)->get();
 		// passing data pegawai yang didapat ke view edit.blade.php
 		return view('pegawai.edit',['pegawai' => $pegawai]);
-
 
 	}
 
@@ -71,5 +76,19 @@ class PegawaiController extends Controller
 
 		// alihkan halaman ke halaman pegawai
 		return redirect('/pegawai');
+	}
+    public function cari(Request $request)
+	{
+		// menangkap data pencarian
+		$cari = $request->cari;
+
+    		// mengambil data dari table pegawai sesuai pencarian data
+		$pegawai = DB::table('pegawai')
+		->where('pegawai_nama','like',"%".$cari."%")
+		->paginate();
+
+    		// mengirim data pegawai ke view index
+		return view('index2',['pegawai' => $pegawai, 'cari' => $cari]);
+
 	}
 }
--- resources/views/pegawai/tambah.blade.php
+++ resources/views/pegawai/tambah.blade.php
@@ -1,30 +1,48 @@
-<!DOCTYPE html>
-<html>
+@extends('template')
 
-<head>
-    <title>Tutorial Membuat CRUD Pada Laravel - www.malasngoding.com</title>
-</head>
+@section('content')
+	<h3>Data Pegawai</h3>
 
-<body>
+	<a href="/pegawai" class="btn btn-info"> Kembali</a>
 
-    <h2><a href="https://www.malasngoding.com">www.malasngoding.com</a></h2>
-    <h3>Data Pegawai</h3>
+	<br/>
+	<br/>
 
-    <a href="/pegawai"> Kembali</a>
+	<form action="/pegawai/store" method="post">
+		{{ csrf_field() }}
+        <div class="row">
+            <div class="col-3">
+                Nama
+            </div>
+            <div class="col-8">
+                <input type="text" name="nama" required="required" class="form-control">
+            </div>
+        </div>
+        <div class="row">
+            <div class="col-3">
+                Jabatan
+            </div>
+            <div class="col-8">
+                <input type="text" name="jabatan" required="required" class="form-control">
+            </div>
+        </div>
+		<div class="row">
+            <div class="col-3">
+                Umur
+            </div>
+            <div class="col-8">
+                <input type="number" name="umur" required="required" class="form-control">
+            </div>
+        </div>
+        <div class="row">
+            <div class="col-3">
+                Alamat
+            </div>
+            <div class="col-8">
+                <textarea name="alamat" required="required" class="form-control"></textarea>
+            </div>
+        </div>
+		<input type="submit" value="Simpan Data" class="btn btn-success">
+	</form>
 
-    <br />
-    <br />
-
-    <form action="/pegawai/store" method="post">
-        {{ csrf_field() }}
-        Nama <input type="text" name="nama" required="required"> <br />
-        Jabatan <input type="text" name="jabatan" required="required"> <br />
-        Umur <input type="number" name="umur" required="required"> <br />
-        Alamat
-        <textarea name="alamat" required="required"></textarea> <br />
-        <input type="submit" value="Simpan Data">
-    </form>
-
-</body>
-
-</html>
+@endsection
